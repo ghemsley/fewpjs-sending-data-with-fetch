de313085d0b7cc567121df2fbe25c64a84b7f231
ghemsley <ghemsley@protonmail.ch>
2021-05-13 20:28:50 -0500

    commit tree e5fb92b1a4cc7eabac44c454a189d4c8a47f51a4

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const submitData = () => {
+  return fetch('http://localhost:3000/users', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({
+      name: 'Steve',
+      email: 'steve@steve.com'
+    })
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .then((json) => {
+      console.log(json)
+      const p = document.createElement('p')
+      p.textContent = json.id
+      document.body.appendChild(p)
+    })
+    .catch((error) => {
+      console.log(error)
+      const p = document.createElement('p')
+      p.textContent = error.message
+      document.body.appendChild(p)
+    })
+}
